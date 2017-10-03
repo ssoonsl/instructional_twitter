7665c0286e777a0da5be4c6d9c2d756075496efc
ssoonsl <ssoonsl616+gh@gmail.com>
2017-10-03 22:25:53 +0800

    Add reply functionality to tweets#show

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,10 @@ class TweetsController < ApplicationController
     @tweets = Tweet.order(created_at: :asc)
   end
 
+  def show
+    @tweet = Tweet.find(params[:id])
+    @replies = @tweet.replies
+    @reply = Reply.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :tweets, only: [:index]
+  resources :tweets, only: [:index, :show] do
+    resources :replies, only: [:create]
+  end
 
   root to: 'tweets#index'
 
